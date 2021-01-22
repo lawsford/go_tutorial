8d26a0043dc8f21c1cc7fc85e8fa717a10d23bc0
Sarim Asif <msasef18@gmail.com>
2021-01-22 23:32:49 +0100

    add hello world script

---
+++ basic/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt" // import standard library package
+
+import "rsc.io/quote" // import external package
+
+func main() { // implement main function
+	fmt.Println(quote.Go())
+}

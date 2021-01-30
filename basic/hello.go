d4dfc080d69617e68117148c68a71ed5ab42b793
Sarim Asif <msasef18@gmail.com>
2021-01-30 03:06:11 +0100

    add more code from tutorial

--- basic/hello.go
+++ basic/hello.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt" // import standard library package
+import (
+    "fmt"
+    "log"
 
-import "rsc.io/quote" // import external package
+    "example.com/greetings"
+)
 
-func main() { // implement main function
-	fmt.Println(quote.Go())
+func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("Lawsford")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(message)
 }
